efb1984654f310b47723360fbaaf86fd95f968f0
Yurbus <frankins636@gmail.com>
2023-04-07 11:30:28 +0300

    second branch

--- src/js/languageToggle.js
+++ src/js/languageToggle.js
@@ -1,15 +1,15 @@
-const languageMenu = document.querySelector('.language-menu');
-const languageToggle = languageMenu.querySelector('.language-toggle');
-const languageList = languageMenu.querySelector('.language-list');
+// const languageMenu = document.querySelector('.language-menu');
+// const languageToggle = languageMenu.querySelector('.language-toggle');
+// const languageList = languageMenu.querySelector('.language-list');
 
-languageToggle.addEventListener('click', function () {
-  languageMenu.classList.toggle('open');
-});
+// languageToggle.addEventListener('click', function () {
+//   languageMenu.classList.toggle('open');
+// });
 
-languageList.addEventListener('click', function (event) {
-  event.preventDefault();
-  const lang = event.target.getAttribute('lang');
-  document.documentElement.lang = lang;
-  languageToggle.textContent = event.target.textContent;
-  languageMenu.classList.remove('open');
-});
+// languageList.addEventListener('click', function (event) {
+//   event.preventDefault();
+//   const lang = event.target.getAttribute('lang');
+//   document.documentElement.lang = lang;
+//   languageToggle.textContent = event.target.textContent;
+//   languageMenu.classList.remove('open');
+// });
