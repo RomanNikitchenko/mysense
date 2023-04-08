4fe4b822391c395e3722474ddd9032650c82379e
RomanNikitchenko <88657590+RomanNikitchenko@users.noreply.github.com>
2023-04-08 15:53:03 +0300

    header

--- src/js/languageToggle.js
+++ src/js/languageToggle.js
@@ -2,9 +2,11 @@ const languageMenu = document.querySelector('.language-menu');
 const languageToggle = languageMenu.querySelector('.language-toggle');
 const language = languageToggle.querySelector('.language');
 const languageList = languageMenu.querySelector('.language-list');
+const iconArrow = languageToggle.querySelector('.icon-arrow');
 
 languageToggle.addEventListener('click', function () {
   languageMenu.classList.toggle('open');
+  iconArrow.classList.toggle('rotate');
 });
 
 languageList.addEventListener('click', function (event) {
@@ -13,4 +15,5 @@ languageList.addEventListener('click', function (event) {
   document.documentElement.lang = lang;
   language.textContent = event.target.textContent;
   languageMenu.classList.remove('open');
+  iconArrow.classList.remove('rotate');
 });
