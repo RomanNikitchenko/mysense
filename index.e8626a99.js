8857aa13c0ae28874b009833e60be46d1d1e4098
RomanNikitchenko <88657590+RomanNikitchenko@users.noreply.github.com>
2023-04-09 12:17:24 +0000

    Deploying to gh-pages from @ RomanNikitchenko/mysense@7ba43537bb7b086dc288dfbd1f4d68803c096231 🚀

--- index.e8626a99.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".language-menu"),t=e.querySelector(".language-toggle"),n=t.querySelector(".language"),a=e.querySelector(".language-list"),o=t.querySelector(".icon-arrow");t.addEventListener("click",(function(){e.classList.toggle("open"),o.classList.toggle("rotate")})),a.addEventListener("click",(function(t){t.preventDefault();var a=t.target.getAttribute("lang");document.documentElement.lang=a,n.textContent=t.target.textContent,e.classList.remove("open"),o.classList.remove("rotate")}))}();
-//# sourceMappingURL=index.e8626a99.js.map
