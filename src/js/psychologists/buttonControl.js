3978d697e15c6d2eec0b07798d7f345471397892
RomanNikitchenko <88657590+RomanNikitchenko@users.noreply.github.com>
2023-04-20 22:47:57 +0300

    employees

--- src/js/psychologists/buttonControl.js
+++ src/js/psychologists/buttonControl.js
@@ -1,57 +1,32 @@
-let employees = [
-  { name: 'Иван', skills: ['Чоловіча', 'Російська', 'Англійська'] },
-  { name: 'Анна', skills: ['Жіноча', 'Англійська', 'Українська'] },
-  { name: 'Петр', skills: ['Чоловіча', 'Російська', 'Українська'] },
-  { name: 'Мария', skills: ['Жіноча', 'Українська', 'Російська'] },
-  { name: 'Дмитрий', skills: ['Чоловіча', 'Українська', 'Російська'] },
-];
-
-function filterEmployeesByAllSkills(employees, selectedSkills) {
-  if (selectedSkills.length === 0) {
-    return employees;
-  }
-  return employees.filter(employee => {
-    return selectedSkills.every(skill => employee.skills.includes(skill));
-  });
-}
-
 (() => {
   const buttons = document.querySelectorAll(
     '.psychologists-section__filter-btn'
   );
 
   // Создаем массив для хранения ID выбранных пользователем кнопок
-  let selectedButtonIds = [];
-
-  let filteredEmployees = filterEmployeesByAllSkills(
-    employees,
-    selectedButtonIds
-  );
-  console.log(filteredEmployees);
+  let selectedButtonValues = [];
 
   buttons.forEach(button => {
     button.addEventListener('click', () => {
-      // Получаем ID кнопки
-      const buttonId = button.getAttribute('data-button-id');
+      // Получаем value кнопки
+      const buttonValue = button.getAttribute('data-button-value');
 
-      // Проверяем, есть ли уже такой ID в массиве
-      if (selectedButtonIds.includes(buttonId)) {
-        // Если есть, удаляем его из массива
-        selectedButtonIds = selectedButtonIds.filter(id => id !== buttonId);
+      // Проверяем, есть ли уже такой value в массиве
+      // Если есть, удаляем его из массива
+      // Если такого value нет, добавляем его в массив
+
+      if (selectedButtonValues.includes(buttonValue)) {
+        selectedButtonValues = selectedButtonValues.filter(
+          value => value !== buttonValue
+        );
       } else {
-        // Если такого ID нет, добавляем его в массив
-        selectedButtonIds.push(buttonId);
+        selectedButtonValues.push(buttonValue);
       }
 
       // Добавляем/удаляем класс "active" на кнопке
       button.classList.toggle('active');
 
-      let filteredEmployees = filterEmployeesByAllSkills(
-        employees,
-        selectedButtonIds
-      );
-
-      console.log(filteredEmployees);
+      console.log(selectedButtonValues);
     });
   });
 })();
