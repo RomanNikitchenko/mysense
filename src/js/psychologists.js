95036b22cd456639c0ce01583f57396980c7044c
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2023-04-21 17:03:47 +0300

    psychologists-our-team

--- src/js/psychologists.js
+++ src/js/psychologists.js
@@ -3,3 +3,4 @@ import './mobile-menu';
 import './psychologists/theme-menu-handler';
 import './psychologists/mobile-menu-filter';
 import './psychologists/buttonControl';
+import './psychologists/our-team-card';
