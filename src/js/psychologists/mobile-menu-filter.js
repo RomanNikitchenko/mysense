5b8d03d6416ff85ae099f7c9096e323f826cf716
RomanNikitchenko <88657590+RomanNikitchenko@users.noreply.github.com>
2023-04-22 13:08:21 +0300

    delete bodyScrollLock

--- src/js/psychologists/mobile-menu-filter.js
+++ src/js/psychologists/mobile-menu-filter.js
@@ -1,31 +1,27 @@
-import * as bodyScrollLock from 'body-scroll-lock';
+(() => {
+  const body = document.querySelector('body');
+  const beckdropMobileMenuFilter = document.querySelector(
+    '.js-menu-container-filter'
+  );
+  const openMenuBtnFilter = document.querySelector('.js-open-menu-filter');
+  const closeMenuBtnFilter = document.querySelector('.js-close-menu-filter');
 
-const beckdropMobileMenuFilter = document.querySelector(
-  '.js-menu-container-filter'
-);
-const openMenuBtnFilter = document.querySelector('.js-open-menu-filter');
-const closeMenuBtnFilter = document.querySelector('.js-close-menu-filter');
+  const toggleMenu = () => {
+    const isMenuOpenFilter =
+      openMenuBtnFilter.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtnFilter.setAttribute('aria-expanded', !isMenuOpenFilter);
+    beckdropMobileMenuFilter.classList.toggle('is-open');
+    body.classList.toggle('modal-is-open');
+  };
 
-const toggleMenu = () => {
-  const isMenuOpenFilter =
-    openMenuBtnFilter.getAttribute('aria-expanded') === 'true' || false;
-  openMenuBtnFilter.setAttribute('aria-expanded', !isMenuOpenFilter);
-  beckdropMobileMenuFilter.classList.toggle('is-open');
+  openMenuBtnFilter.addEventListener('click', toggleMenu);
+  closeMenuBtnFilter.addEventListener('click', toggleMenu);
 
-  const scrollLockMethod = !isMenuOpenFilter
-    ? 'disableBodyScroll'
-    : 'enableBodyScroll';
-  bodyScrollLock[scrollLockMethod](document.body);
-};
-
-openMenuBtnFilter.addEventListener('click', toggleMenu);
-closeMenuBtnFilter.addEventListener('click', toggleMenu);
-
-// Закрываем мобильное меню на более широких экранах
-// в случае изменения ориентации устройства.
-window.matchMedia('(min-width: 1280px)').addEventListener('change', e => {
-  if (!e.matches) return;
-  beckdropMobileMenuFilter.classList.remove('is-open');
-  openMenuBtnFilter.setAttribute('aria-expanded', false);
-  bodyScrollLock.enableBodyScroll(document.body);
-});
+  // Закрываем мобильное меню на более широких экранах
+  // в случае изменения ориентации устройства.
+  window.matchMedia('(min-width: 1280px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    beckdropMobileMenuFilter.classList.remove('is-open');
+    openMenuBtnFilter.setAttribute('aria-expanded', false);
+  });
+})();
