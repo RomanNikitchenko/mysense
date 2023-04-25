50b9071e2c075fc13828b71f6db9cbc2cde5cbed
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2023-04-25 19:40:16 +0300

    test-page-section-visibility

--- src/js/tests/tests.js
+++ src/js/tests/tests.js
@@ -10,3 +10,62 @@ itemWraps.forEach(itemWrap => {
     }
   });
 });
+
+//section
+const sectionTestPage = document.querySelector('.testpage');
+const testCardWrapBtn = document.querySelectorAll('.test__card-wrap');
+const fixedBox = document.querySelector('.fixed__box');
+const sectionState = document.querySelector('.state');
+const sectionRequests = document.querySelector('.requests');
+const sectionConsern = document.querySelector('.consern');
+const sectionCategory = document.querySelector('.category');
+
+fixedBox.classList.add('state--is-hidden');
+sectionState.classList.add('state--is-hidden');
+sectionRequests.classList.add('state--is-hidden');
+sectionConsern.classList.add('state--is-hidden');
+sectionCategory.classList.add('state--is-hidden');
+
+let myself = false;
+let family = false;
+let child = false;
+
+testCardWrapBtn.forEach(item => {
+  item.addEventListener('click', e => {
+    if (e.currentTarget.dataset.for === 'myself') {
+      console.log('myself');
+      myself = true;
+      family = false;
+      child = false;
+    }
+
+    if (e.currentTarget.dataset.for === 'family') {
+      console.log('family');
+      myself = false;
+      family = true;
+      child = false;
+    }
+
+    if (e.currentTarget.dataset.for === 'child') {
+      console.log('child');
+      myself = false;
+      family = false;
+      child = true;
+    }
+  });
+});
+
+const fixedButtonPrew = document.querySelector('.fixed__button-prew');
+const fixedButtonNext = document.querySelector('.fixed__button-next');
+
+fixedButtonNext.addEventListener('click', () => {
+  if (myself) {
+    console.log('myself');
+  }
+  if (family) {
+    console.log('family');
+  }
+  if (child) {
+    console.log('child');
+  }
+});
