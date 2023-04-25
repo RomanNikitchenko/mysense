9802654956d9637c3f6df9b2d127950cb956594f
RomanNikitchenko <88657590+RomanNikitchenko@users.noreply.github.com>
2023-04-26 00:50:09 +0300

    is-active

--- src/js/tests/tests.js
+++ src/js/tests/tests.js
@@ -12,7 +12,7 @@ itemWraps.forEach(itemWrap => {
 });
 
 //section
-const sectionTestPage = document.querySelector('.testpage');
+const testpageContainer = document.querySelector('.testpage__container');
 const testCardWrapBtn = document.querySelectorAll('.test__card-wrap');
 const fixedBox = document.querySelector('.fixed__box');
 const sectionState = document.querySelector('.state');
@@ -20,38 +20,47 @@ const sectionRequests = document.querySelector('.requests');
 const sectionConsern = document.querySelector('.consern');
 const sectionCategory = document.querySelector('.category');
 
-fixedBox.classList.add('state--is-hidden');
-sectionState.classList.add('state--is-hidden');
-sectionRequests.classList.add('state--is-hidden');
-sectionConsern.classList.add('state--is-hidden');
-sectionCategory.classList.add('state--is-hidden');
+sectionState.style.display = 'none';
+sectionRequests.style.display = 'none';
+sectionConsern.style.display = 'none';
+sectionCategory.style.display = 'none';
+fixedBox.style.display = 'none';
 
 let myself = false;
 let family = false;
 let child = false;
+let page = 0;
 
 testCardWrapBtn.forEach(item => {
   item.addEventListener('click', e => {
+    for (let i = 0; i < testCardWrapBtn.length; i += 1) {
+      if (testCardWrapBtn[i].classList.contains('is-active')) {
+        testCardWrapBtn[i].classList.remove('is-active');
+        break;
+      }
+    }
+
     if (e.currentTarget.dataset.for === 'myself') {
-      console.log('myself');
       myself = true;
       family = false;
       child = false;
     }
 
     if (e.currentTarget.dataset.for === 'family') {
-      console.log('family');
       myself = false;
       family = true;
       child = false;
     }
 
     if (e.currentTarget.dataset.for === 'child') {
-      console.log('child');
       myself = false;
       family = false;
       child = true;
     }
+
+    item.classList.add('is-active');
+
+    fixedBox.style.display = 'flex';
   });
 });
 
@@ -59,13 +68,38 @@ const fixedButtonPrew = document.querySelector('.fixed__button-prew');
 const fixedButtonNext = document.querySelector('.fixed__button-next');
 
 fixedButtonNext.addEventListener('click', () => {
-  if (myself) {
-    console.log('myself');
+  if (page === 0) {
+    page += 1;
+
+    testpageContainer.style.display = 'none';
+
+    if (myself) {
+      sectionState.style.display = 'block';
+      return;
+    }
+    if (family) {
+      sectionConsern.style.display = 'block';
+      return;
+    }
+    if (child) {
+      sectionCategory.style.display = 'block';
+      return;
+    }
   }
-  if (family) {
-    console.log('family');
-  }
-  if (child) {
-    console.log('child');
+
+  if (page === 1) {
+    if (myself) {
+      sectionState.style.display = 'none';
+      sectionRequests.style.display = 'block';
+      return;
+    }
+    if (family) {
+      sectionConsern.style.display = 'block';
+      return;
+    }
+    if (child) {
+      sectionCategory.style.display = 'block';
+      return;
+    }
   }
 });
