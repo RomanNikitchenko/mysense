2d23ff3702df872ee579bbd6bcdbbbad790f3dd9
RomanNikitchenko <88657590+RomanNikitchenko@users.noreply.github.com>
2023-05-07 15:02:29 +0300

    setPaddingRight

--- src/js/psycholog-exemple/changingPageElements.js
+++ src/js/psycholog-exemple/changingPageElements.js
@@ -13,7 +13,10 @@
     }
   };
 
-  console.log(load('item'));
+  if (!load('item')) {
+    return;
+  }
+
   const { name, experience, photoSrc, priceValue } = load('item');
 
   cardImg.src = photoSrc;
@@ -21,3 +24,33 @@
   cardInfoExperience.innerHTML = experience;
   cardPrice.innerHTML = `${priceValue}<span>грн/год</span>`;
 })();
+
+document.addEventListener('DOMContentLoaded', function () {
+  setPaddingRight(); // вызываем функцию после загрузки всех элементов страницы
+});
+
+function setPaddingRight() {
+  const sliderSectionList = document.querySelector(
+    '.psychologists-slider-section__list'
+  );
+  let width = window.innerWidth;
+  let paddingRight;
+
+  if (width >= 1280) {
+    sliderSectionList.style.paddingRight = `${20}px`;
+  } else if (width >= 744) {
+    sliderSectionList.style.paddingRight = `${20}px`;
+  } else {
+    if (width <= 390) {
+      paddingRight = 20;
+    } else {
+      const extraWidth = width - 390;
+      const additionalPadding = Math.floor(extraWidth / 20) * 0.5;
+      paddingRight = 20 + Math.floor(extraWidth / 2) + additionalPadding;
+    }
+  }
+
+  sliderSectionList.style.paddingRight = `${paddingRight}px`;
+}
+
+window.addEventListener('resize', setPaddingRight);
