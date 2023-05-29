ed555a2df33f40525808672c618e0cb10e945362
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2023-05-29 15:48:37 +0300

    person-info-button

--- src/js/private-office.js
+++ src/js/private-office.js
@@ -3,3 +3,4 @@ import './mobile-menu';
 import './private-office/pageHeaderShowMenu';
 import './private-office/mobileMenuPrivate';
 import './private-office/switchBetweenListAndCalendar';
+import './private-office/modalUserInformation';
