8dcb334af4dba739c68102254347e170c47eb4b4
RomanNikitchenko <88657590+RomanNikitchenko@users.noreply.github.com>
2023-06-07 22:44:39 +0300

    bag-fix

---
+++ src/js/private-office/checkLoggedIn.js
@@ -0,0 +1,26 @@
+const pageHeaderPrivateOfficeMenu = document.querySelector(
+  '.page-header__private-office-menu'
+);
+const login = document.querySelector('.login');
+const choosePerson = document.querySelector('.choose-person');
+
+// Функция для проверки, вошел ли пользователь в систему
+checkLoggedIn();
+
+function checkLoggedIn() {
+  // Проверяем, есть ли информация о текущем пользователе в локальном хранилище
+  var currentUser = JSON.parse(localStorage.getItem('currentUser'));
+
+  if (currentUser) {
+    pageHeaderPrivateOfficeMenu.classList.remove('visually-hidden');
+    login.classList.add('visually-hidden');
+    choosePerson.classList.add('visually-hidden');
+    // Вы можете выполнить перенаправление на другую страницу
+  } else {
+    pageHeaderPrivateOfficeMenu.classList.add('visually-hidden');
+    login.classList.remove('visually-hidden');
+    choosePerson.classList.remove('visually-hidden');
+    window.location.assign('registration_form.html');
+    // Вы можете выполнить перенаправление на страницу входа или другую страницу
+  }
+}
