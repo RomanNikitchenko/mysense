4ade5d1cd0fc0e1ebcbe9e7b3626cdf131f20b5e
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2023-06-19 13:36:04 +0300

    Cleave

--- src/js/client-office/formAddNewPaymentMethodInput.js
+++ src/js/client-office/formAddNewPaymentMethodInput.js
@@ -1,13 +1,23 @@
 import Cleave from 'cleave.js/dist/cleave.min.js';
 
 (() => {
-  const addNewPaymentMethodInput = document.getElementById(
-    'add-new-payment-method'
-  );
+  const cardNumberInput = document.getElementById('card-number');
+  const expiryInput = document.getElementById('expiry');
+  const cvvInput = document.getElementById('cvv');
 
-  let cleave = new Cleave(addNewPaymentMethodInput, {
+  const cardNumberCleave = new Cleave(cardNumberInput, {
     creditCard: true,
     delimiter: ' ',
     blocks: [4, 4, 4, 4],
+  });
+
+  const expiryCleave = new Cleave(expiryInput, {
+    date: true,
+    datePattern: ['m', 'y'],
+  });
+
+  const cvvCleave = new Cleave(cvvInput, {
+    numeral: true,
+    blocks: [3],
   });
 })();
