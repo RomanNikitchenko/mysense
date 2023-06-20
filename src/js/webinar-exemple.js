5808a0f1bf63b533dcb12ff9515f007f2459b93d
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2023-06-20 18:36:33 +0300

    webinar page hero

---
+++ src/js/webinar-exemple.js
@@ -0,0 +1,8 @@
+import './languageToggle';
+import './mobile-menu';
+import './private-office/modalInformationDepartment';
+import './private-office/modalReminder';
+import './checkLoggedIn';
+import './private-office/outsidePrivateOffice';
+import './private-office/outsideMenuNavigation';
+import './private-office/outsideLogout';
