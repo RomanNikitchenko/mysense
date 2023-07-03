ca7c94352642107d0ef90e632e92a5e995b1793e
RomanNikitchenko <88657590+RomanNikitchenko@users.noreply.github.com>
2023-07-03 12:43:10 +0000

    Deploying to gh-pages from @ RomanNikitchenko/mysense@7cca78df19d7c26a1bba980019c2da381c527dce 🚀

---
+++ post-exemple.ec9007b9.js
@@ -0,0 +1,2 @@
+$(".slider").slick({dots:!0,infinite:!0,speed:300,slidesToShow:2,slidesToScroll:1,arrows:!0,responsive:[{breakpoint:1170,settings:{slidesToShow:2,slidesToScroll:1,infinite:!0,dots:!0}},{breakpoint:768,settings:{slidesToShow:1,slidesToScroll:1}}]});
+//# sourceMappingURL=post-exemple.ec9007b9.js.map
